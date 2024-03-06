52fd4f5d4f757620ed670da98328f6b4ab8923d4
ugyobppang <yeonguk0517@gmail.com>
2024-03-06 20:18:07 +0900

    style: TodoSubmit 컴포넌트 UI 구현

--- src/components/todo/TodoSubmit.tsx
+++ src/components/todo/TodoSubmit.tsx
@@ -1,13 +1,47 @@
+import styled from 'styled-components';
+
 export default function TodoSubmit() {
   return (
     <section>
-      <form>
+      <FormArea>
         <label>제목 : </label>
         <input type="text" name="title" />
         <label>내용 : </label>
         <input type="text" name="content" />
         <button>추가하기</button>
-      </form>
+      </FormArea>
     </section>
   );
 }
+
+const FormArea = styled.form`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 2rem;
+  padding: 1.5rem 1rem;
+  & label {
+    font-weight: bold;
+  }
+  & input {
+    margin-left: -1.6rem;
+    border: none;
+    border-bottom: 2px solid black;
+    width: 280px;
+    height: 30px;
+    outline: none;
+  }
+  & button {
+    border: none;
+    border-radius: 5px;
+    background-color: #00ff00;
+    width: 150px;
+    height: 30px;
+    margin-left: auto;
+    transition: all 0.2s ease-in-out;
+    font-weight: bold;
+  }
+  & button:hover {
+    background-color: #00e900;
+  }
+`;
