9ece3e9d7130fc01f2d825acc39eb9750146cfc0
ugyobppang <yeonguk0517@gmail.com>
2024-03-06 20:03:42 +0900

    TodoController 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import TodoController from './components/todo/TodoController';
 
 export default function App() {
   return (
     <>
       <Header />
+      <TodoController />
     </>
   );
 }
---
+++ src/components/todo/TodoController.tsx
@@ -0,0 +1,3 @@
+export default function TodoController() {
+  return <main>TodoController</main>;
+}
